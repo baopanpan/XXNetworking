990381f3cf94b1ab27676eba4e26aa8f42af94e1
R <R@lurongdeiMac.local>
2018-09-06 14:38:55 +0800

    Initial commit

---
+++ Example/XXNetworking/BPPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BPPAppDelegate.h
+//  XXNetworking
+//
+//  Created by baopanpan on 09/06/2018.
+//  Copyright (c) 2018 baopanpan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BPPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/XXNetworking/BPPViewController.h
@@ -0,0 +1,13 @@
+//
+//  BPPViewController.h
+//  XXNetworking
+//
+//  Created by baopanpan on 09/06/2018.
+//  Copyright (c) 2018 baopanpan. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BPPViewController : UIViewController
+
+@end
